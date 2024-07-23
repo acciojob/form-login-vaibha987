c18f8204f62e0668693a7aff53f7d3d5ca3bda80
vaibha987 <v2goyal95@gmail.com>
2024-07-23 12:50:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	let firstName=document.querySelector("#firstName")
+	let lastName=document.querySelector("#lastName")
+	console.log(firstName.value+" "+lastName.value)
 
 }
